764e67a95e495d46cfd4dbe8d7b122724479cdc8
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-10-14 19:33:31 +0100

    Updated refs

--- main.js
+++ main.js
@@ -4,17 +4,18 @@ Websites = new Map();
 Websites.set('https://www.microsoft.com/favicon.ico?v2', "Microsoft")
 .set("https://github.com/manifest.json", "Github")
 .set("https://discordapp.com/assets/07dca80a102d4149e9736d4b162cff6f.ico", "Discord")
-.set("https://www.yahoo.com/favicon.ico", "Yahoo")
+.set("https://s.yimg.com/rz/l/favicon.ico", "Yahoo")
 .set("https://outlook.live.com/owa/favicon.ico", "Outlook")
 .set("https://combo.staticflickr.com/pw/favicon.ico", "Flicker")
 .set("https://static.xx.fbcdn.net/rsrc.php/yo/r/iRmz9lCMBD2.ico", "Facebook")
 .set("https://twitter.com/manifest.json", "Twitter")
 .set("https://ndev.tk/favicon.ico", "NVDev")
 .set("https://www.bing.com/sa/simg/bing_p_rr_teal_min.ico", "Bing")
-.set("https://www.redditstatic.com/desktop2x/img/favicon/manifest.json", "Reddit")
+.set("https://www.redditstatic.com/desktop2x/img/banner/redditor@2x.png", "Reddit")
 .set("https://youtube.com/manifest.json", "Youtube")
 .set("https://store.steampowered.com/favicon.ico", "Steam")
 .set("https://ssl.gstatic.com/ui/v1/icons/mail/images/favicon5.ico", "Google mail")
+.set("https://www.google.com/favicon.ico", "Google")
 
 function is304(res) { // Requires CORS
   if (res.encodedBodySize > 0 &&
