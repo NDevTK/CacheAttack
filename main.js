5a544fb8eb2354c1c6188d206f9f3f5f0092cc75
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-10-14 21:46:48 +0100

    Improvements to website list

--- main.js
+++ main.js
@@ -16,6 +16,16 @@ Websites.set('https://www.microsoft.com/favicon.ico?v2', "Microsoft")
 .set("https://store.steampowered.com/favicon.ico", "Steam")
 .set("https://ssl.gstatic.com/ui/v1/icons/mail/images/favicon5.ico", "Google mail")
 .set("https://www.google.com/favicon.ico", "Google")
+.set("https://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico?v=4f32ecc8f43d", "Stack Overflow")
+.set("https://cdn.sstatic.net/Sites/security/img/favicon.ico?v=7585a77d4575", "Information Security Stack Exchange")
+.set("https://cdn.sstatic.net/Sites/codereview/img/logo.svg?v=0dfb1294dc6e0", "Code Review Stack Exchange")
+.set("https://cdn.sstatic.net/Sites/superuser/img/favicon.ico?v=29b26120d2c5", "Super User")
+.set("https://static-global-s-msn-com.akamaized.net/hp-neu/sc/2b/a5ea21.ico", "MSN")
+.set("https://www.torproject.org/static/images/favicon/favicon.ico", "Tor Project")
+.set("https://duckduckgo.com/favicon.ico", "DuckDuckGo")
+.set("https://secure.skypeassets.com/apollo/2.1.1477/images/icons/favicon.ico", "Skype")
+.set("https://www.amazon.com/favicon.ico", "Amazon")
+.set("https://pages.ebay.com/favicon.ico", "ebay")
 
 function is304(res) { // Requires CORS
   if (res.encodedBodySize > 0 &&
