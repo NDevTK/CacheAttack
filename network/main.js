3a61e40aa3344af13cc56ffaa585bb418a3fb4ed
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-10-19 15:55:54 +0100

    Create main.js

---
+++ network/main.js
@@ -0,0 +1,39 @@
+ClassC(["192.168.1.", "192.168.0.", "10.0.0."], Checker);
+
+async function Checker(ip) {
+    let state = await isAlive(ip);
+    if(state) addData(ip); 
+}
+
+async function ClassC(array, callback) {
+    array.forEach(prefix => {
+        for (var i = 1; i <= 255; i++) {
+            callback(prefix + i)
+        }
+    })
+}
+
+async function wait(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function isAlive(ip) {
+    try {
+    var img = new Image(0, 0);
+    img.hidden = true;
+    img.src = "http://"+ip;
+    document.body.appendChild(img);
+    } catch(err) {}
+    await wait(30);
+    let data = img.complete;
+    img.remove();
+    return data;
+}
+
+async function addData(ip) {
+    dataTable.hidden = false;
+    info.hidden = true;
+    data.insertRow(0).insertCell(0).innerText = ip;
+}
