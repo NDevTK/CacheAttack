bfc7c33eded5e0d16149ffcc295519c5ca790604
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-10-19 12:23:01 +0100

    Create index.js

---
+++ network/index.js
@@ -0,0 +1,27 @@
+
+basicScan(["192.168.1.", "192.168.0."], addData);
+
+async function Performance(url){
+  var img = new Image(0,0);
+  img.hidden = true;
+  img.src = url;
+  document.body.appendChild(img); 
+  await wait(max);
+  let data = img.complete;
+  img.remove();
+  return data;
+}
+
+async function ClassC(array, callback) {
+    array.forEach(prefix => {
+        for (var i = 1; i <= 255; i++) {
+            callback(start + i)
+        }
+    })
+}
+
+async function addData(displayName) {
+    dataTable.hidden = false;
+    info.hidden = true;
+    data.insertRow(0).insertCell(0).innerText = displayName;
+}
