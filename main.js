5c5ea9196b1d3d75c1d8d0a15bf0c24b292d9bca
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-11-03 14:48:02 +0000

    Using callback

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 // NDev 2019 https://github.com/NDevTK/CacheAttack
-const cache_test = "https://ndev.tk/README.md?".concat(Math.random());
 
 document.addEventListener('DOMContentLoaded', async _ => {
     setTimeout(async _ => {
-            // Foreach website check if cached
-            getWebsites().then(result => {
-                result.forEach(displayName => {
-                    addData(displayName);
-                });
-                if (dataTable.hidden === true) info.innerText = "No result found :(";
+            await getWebsites(displayName => addData(displayName));
+            if (dataTable.hidden === true) info.innerText = "No result found :(";
             });
     }, 150)
 });
