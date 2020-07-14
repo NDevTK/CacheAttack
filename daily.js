9ada71d2ab6b2e904c780c42ba9ec4c78ddea27e
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-07-14 17:26:38 +0100

    Update daily.js

--- daily.js
+++ daily.js
@@ -8,12 +8,17 @@ async function getChannels() {
   let channels = await r.json();
   var output = [];
   for (channel of channels) {
+    try {
     let r = await fetch("https://invidio.us/api/v1/search?type=channel&q="+encodeURI(channel));
     if(r.status !== 200) continue
     let result = await r.json();
+    if(result.length === 0) continue
     let channelData = result[0];
     let url = channelData.authorThumbnails[0].url.replace("c0x00ffffff", "c0xffffffff");
     output.push([channelData.authorId, url]);
+    } catch {
+      console.log("Error parsing API: "+channel);
+    }
   };
   fs.writeFileSync('channels', output);
 }
