47d6234f5e55f2b95bfc27d350598bf827b76163
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-07-14 17:16:39 +0100

    Update daily.js

--- daily.js
+++ daily.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-let r = await fetch("https://cors.usercontent.ndev.tk/channels");
-let channels = await r.json();
+getChannels();
 
-for (channel of channels) {
-  let r = await fetch("https://invidio.us/api/v1/search?type=channel&q="+encodeURI(channel));
-  if(r.status !== 200) continue
-  let result = await r.json();
-  let channelData = result[0];
-  let url = channelData.authorThumbnails[0].url.replace("c0x00ffffff", "c0xffffffff");
-  output.push([channelData.authorId, url]);
-};
-
-fs.writeFileSync('channels', output);
+async function getChannels() {
+  let r = await fetch("https://cors.usercontent.ndev.tk/channels");
+  let channels = await r.json();
+  for (channel of channels) {
+    let r = await fetch("https://invidio.us/api/v1/search?type=channel&q="+encodeURI(channel));
+    if(r.status !== 200) continue
+    let result = await r.json();
+    let channelData = result[0];
+    let url = channelData.authorThumbnails[0].url.replace("c0x00ffffff", "c0xffffffff");
+    output.push([channelData.authorId, url]);
+  };
+  fs.writeFileSync('channels', output);
+}
