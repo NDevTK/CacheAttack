bbd02d140bd0ddb325a69ce7a923c444428fe0da
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-07-18 00:29:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', async _ => {
     setTimeout(async _ => {
             await getWebsites(displayName => addData(displayName));
             if (dataTable.hidden === true) info.innerText = "No result found :(";
-    }, 150)
+    }, 2000)
 });
 
 async function addData(displayName) {
