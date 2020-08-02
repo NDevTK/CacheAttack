c635624eea9c09b8305f54baecb0b06a8c9af816
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-08-02 17:41:17 +0100

    Update embed.js

--- embed.js
+++ embed.js
@@ -170,7 +170,7 @@ async function ifCached_3(url) {
 
 async function block(url) {
     for (;;) {
-        if(ifrm.closed) return
+        if(checker.closed) return
         checker.postMessage(url);
         checker.location = "https://cache.ndev.tk/window.html";
         await WindowEvent();
