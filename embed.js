c1f41fa982091424b154894f8f5661c4a6e8d397
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-09-16 17:39:14 +0100

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,54 +1,59 @@
 /*jshint esversion: 8 */
 
 // NDev 2020 https://github.com/NDevTK/CacheAttack
-
 const firefox = navigator.userAgent.includes("Firefox");
 
 const max = 10.5;
 
-if(self.document === undefined) {
+if (self.document === undefined) {
     onmessage = async e => {
-        let result = await ifCachedWorker(e.data);
-        postMessage(result);
+        let result = await ifCachedWorker(e.data, callback => postMessage(result));
+        postMessage("done");
     };
 }
 
-async function getWebsites(cb = null, websites = null) {
+async function getWebsites(cb = false, websites = null) {
     var output = [];
-    if(websites === null) {
+    if (websites === null) {
         websites = await getRules();
     }
     let checks = chunk(websites, Math.ceil(websites.length / navigator.hardwareConcurrency));
     await PromiseForeach(checks, async chunk => {
         let worker = new Worker("https://cache.ndev.tk/embed.js");
         worker.postMessage(chunk);
-        let result = await new Promise(resolve => {worker.onmessage = e => resolve(e.data);});
+        await new Promise(resolve => {
+            worker.onmessage = e => {
+                if (e.data === "done") resolve();
+                if (cb) {
+                    cb(e.data);
+                } else {
+                    output.push(e.data);
+                }
+            };
+        });
         worker.terminate();
-	if(!result || result.length === 0) return;
-	if(cb) result.map(cb);
-        result.map(item => output.push(item));
     }, 600);
     return [...new Set(output)];
 }
 
-async function PromiseForeach(item, callback, delay=0) {
-  var jobs = [];
-  for(let x of item) {
-    await new Promise(resolve => setTimeout(resolve, delay));
-    jobs.push(callback(x));
-  }
-  await Promise.all(jobs);
+async function PromiseForeach(item, callback, delay = 0) {
+    var jobs = [];
+    for (let x of item) {
+        await new Promise(resolve => setTimeout(resolve, delay));
+        jobs.push(callback(x));
+    }
+    await Promise.all(jobs);
 }
 
 function chunk(array, size) {
     const chunked_arr = [];
     for (let i = 0; i < array.length; i++) {
-      const last = chunked_arr[chunked_arr.length - 1];
-      if (!last || last.length === size) {
-        chunked_arr.push([array[i]]);
-      } else {
-        last.push(array[i]);
-      }
+        const last = chunked_arr[chunked_arr.length - 1];
+        if (!last || last.length === size) {
+            chunked_arr.push([array[i]]);
+        } else {
+            last.push(array[i]);
+        }
     }
     return chunked_arr;
 }
@@ -70,28 +75,23 @@ function is304(res) {
 
 function PerformanceCheck(url) {
     var res = performance.getEntriesByName(url).pop();
-    if(res === undefined) return null;
-    if(is304(res)) return true;
+    if (res === undefined) return null;
+    if (is304(res)) return true;
     return (res.transferSize !== 0);
 }
 
-async function ifCachedWorker(websites = null, CacheTest = false) {
-    if(websites === null) {
-        websites = await getRules();
-    }
-    var output = [];
-    if(CacheTest) {
+async function ifCachedWorker(websites, cb, CacheTest = false) {
+    if (CacheTest) {
         let TestResult = await ifCached_test();
-        if(!TestResult) throw "Cache is not working :-(";
+        if (!TestResult) throw "Cache is not working :-(";
     }
     // Foreach website check if cached
     for (let website of websites) {
         let result = await ifCached(website[0]);
-	if(result) {
-	    output.push([website[0], website[1]]);
-	}
+        if (result) {
+            cb(result);
+        }
     }
-    return output;
 }
 
 async function ifCached_test() {
@@ -112,46 +112,46 @@ async function ifCached(url) {
         state = false;
     }, max);
     try {
-        await fetch(url, {mode: "no-cors", credentials: "include", signal});
-    } catch(err) {
-	    // Website blocked by client
-	    clearTimeout(timeout);
-	}
+        await fetch(url, {mode: "no-cors",credentials: "include",signal});
+    } catch (err) {
+        // Website blocked by client
+        clearTimeout(timeout);
+    }
     clearTimeout(timeout);
     return state;
 }
 
 function WindowEvent(check = false) {
-  return new Promise(resolve => {
-    window.addEventListener('message', e => {
-      if(check !== false && e.data !== check) return;
-      resolve(e.data);
+    return new Promise(resolve => {
+        window.addEventListener('message', e => {
+            if (check !== false && e.data !== check) return;
+            resolve(e.data);
+        });
     });
-  });
 }
 
 function initChecker() {
-  checker = open("https://cache.ndev.tk/window.html");
+    checker = open("https://cache.ndev.tk/window.html");
 }
 
 // Currently fails if not in loop
 async function ifCached_window(url) {
-  checker.postMessage(url);
-  let event = await WindowEvent();
-  if(event === "load") {
-    return console.log("Wrong state");
-  }
-  if(event) {
-    checker.location = "https://cache.ndev.tk/window.html";
-  }
-  await WindowEvent("load");
-  if(firefox) await new Promise(resolve => setTimeout(resolve, 50));
-  return event;
+    checker.postMessage(url);
+    let event = await WindowEvent();
+    if (event === "load") {
+        return console.log("Wrong state");
+    }
+    if (event) {
+        checker.location = "https://cache.ndev.tk/window.html";
+    }
+    await WindowEvent("load");
+    if (firefox) await new Promise(resolve => setTimeout(resolve, 50));
+    return event;
 }
 
 async function block(url) {
     for (;;) {
-        if(checker.closed) location.reload();
+        if (checker.closed) location.reload();
         checker.postMessage(url[0]);
         checker.location = "https://cache.ndev.tk/window.html";
         await WindowEvent();
