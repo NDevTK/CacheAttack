247702397d4be84f0405e7f5801ac7c5a082ba61
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-09-27 17:39:05 +0100

    Update ytRules.js

--- ytRules.js
+++ ytRules.js
@@ -1,35 +1,18 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-const cid_regex = /<link rel="canonical" href="https:\/\/www\.youtube\.com\/channel\/([a-z0-9-_]*)">/i;
-const thumbnail_regex = /<link rel="image_src" href="(https:\/\/yt3\.ggpht\.com\/a\/[a-z0-9-_=]*)">/i;
-
 getChannels();
 
 async function getChannels() {
-  var channels = new Map(JSON.parse(fs.readFileSync('channels')));
-  var output = new Set();
+  var channels = [...new Map(JSON.parse(fs.readFileSync('channels'))).keys()];
+  var output = new Map();
   console.info("Fetching top channels");
   //channels.length = 5;
-  console.info("Getting data from Youtube");
-  await PromiseForeach(channels, async (channel, index) => {
-    try {
-    let r = await fetch("https://www.youtube.com/"+encodeURI(channel));
-    if(r.status !== 200) return
-    let result = await r.text();
-    if(result.length === 0) return
-    let cid = result.match(cid_regex)[1];
-    let thumbnail = result.match(thumbnail_regex)[1];
-    if(cid === undefined || thumbnail === undefined) return
-    output.set(cid, thumbnail);
-    } catch {}
-  });
+  for (channel of channels) {
+  let r = await fetch("https://invidio.us/api/v1/channels/"+encodeURI(channel));
+  let data = await r.json();
+  output.set(channel, data.authorThumbnails[0].url.replace("s32", "s400"));
+  }
   console.info("Making file :D");
   fs.writeFileSync('channels', JSON.stringify([...output]));
-}
-
-async function PromiseForeach(item, callback) {
-  var jobs = [];
-  item.forEach(x => jobs.push(callback(x)));
-  await Promise.all(jobs);
 }
