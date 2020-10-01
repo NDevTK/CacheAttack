270d4519bfed391c851ce841a30a7c678fb00a65
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-10-01 22:23:03 +0100

    Update embed.js

--- embed.js
+++ embed.js
@@ -144,7 +144,7 @@ function WindowEvent(check = false) {
 
 function initChecker() {
     checker = open(redirect);
-    window_timeout = (firefox) ? 10 : 31;
+    window_timeout = (firefox) ? 10 : 20;
     if(isTorBrowser()) window_timeout = 150;
 }
 
